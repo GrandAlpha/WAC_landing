1af3361640ee49914af5902549bb67a9be421c4e
Primak <primack.roma@yandex.ru>
2022-12-09 23:47:39 +0300

    Дебаг - false

--- WAC_landing/settings.py
+++ WAC_landing/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'landing.apps.LandingConfig',
+    # 'landing.apps.LandingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- WAC_landing/urls.py
+++ WAC_landing/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
-
 from landing import views
 
 urlpatterns = [
